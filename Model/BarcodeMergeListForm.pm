0a927646a8761a99ed04a5cc981fd4cc32d9fd3e
Rob Nagler <github@q33.us>
2005-11-07 05:11:30 +0000

    detect overlap

--- Model/BarcodeMergeListForm.pm
+++ Model/BarcodeMergeListForm.pm
@@ -13,35 +13,45 @@ sub execute_empty_row {
 
 sub execute_ok_row {
     my($self) = @_;
-    next unless $self->get('want_merge');
+    return unless $self->get('want_merge');
     my($lm) = $self->get_list_model;
     my($to, $from) = map(
 	$self->new_other('RealmOwner')->unauth_load_or_die({
 	    name => $_,
 	}),
 	$lm->get(qw(RealmOwner.name RealmOwner_2.name)));
+    my($req) = $self->get_request;
+    my($prev) = $req->get('auth_realm');
+    $self->new_other('Ride')->do_iterate(
+	sub {
+	    my($r) = @_;
+	    my($v) = {
+		%{$r->get_shallow_copy},
+		user_id => $to->get('realm_id'),
+	    };
+	    $r->unauth_delete;
+	    if ($r->unauth_load($v)) {
+		$self->internal_put_error(want_merge => 'MERGE_OVERLAP');
+		$self->internal_put_field('Ride.ride_date' => $v->{ride_date});
+		return 0;
+	    }
+	    $r->create($v);
+	    return 1;
+	},
+	'unauth_iterate_start',
+	'ride_date',
+	{user_id => $from->get('realm_id')},
+    );
+    return
+	if $self->in_error;
     $self->new_other('RealmUser')->do_iterate(
 	sub {
-	    shift->unauth_delete;
+	    shift->delete;
 	    return 1;
 	},
 	'unauth_iterate_start',
 	'realm_id',
 	{user_id => $from->get('realm_id')},
-    );
-    $self->get_request->set_realm($from);
-#TODO: Check first
-    $self->new_other('Ride')->do_iterate(
-	sub {
-	    my($r) = @_;
-	    $r->create({
-		%{$r->get_shallow_copy},
-		user_id => $to->get('realm_id'),
-	    });
-	    $r->delete;
-	    return 1;
-	},
-	'ride_date',
     );
     $self->new_other('User')->unauth_load_or_die({
 	user_id => $from->get('realm_id'),
@@ -59,6 +69,13 @@ sub internal_initialize {
 		name => 'want_merge',
 		type => 'Boolean',
 		constraint => 'NOT_NULL',
+		in_list => 1,
+	    },
+	],
+	other => [
+	    {
+		name => 'Ride.ride_date',
+		in_list => 1,
 	    },
 	],
     });
