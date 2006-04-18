4f6239f09e294c022968bbb32319553ba32e214f
Rob Nagler <github@q33.us>
2006-04-18 22:14:25 +0000

    vs_prose and vs_main_img

--- ViewShortcuts.pm
+++ ViewShortcuts.pm
@@ -3,15 +3,9 @@
 package Freiker::ViewShortcuts;
 use strict;
 use base 'Bivio::UI::XHTML::ViewShortcuts';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
-our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($AUTOLOAD);
-
-sub AUTOLOAD {
-    return Bivio::UI::ViewLanguage->call_method(
-	$AUTOLOAD, 'Bivio::UI::ViewLanguage', @_,
-    );
-}
 
 sub vs_barcode_ride_link {
     my($self, $field) = @_;
@@ -30,6 +24,18 @@ sub vs_barcode_ride_link {
     }];
 }
 
+sub vs_base_menu {
+    my($proto, $values) = @_;
+    return $proto->vs_call(
+	Join => [map($proto->vs_link(@$_), @$values)],
+	{
+	    join_separator => $proto->vs_call(
+		Image => heart_9 => '', {class => 'sep'},
+	    ),
+	}
+    );
+}
+
 sub vs_gears_email {
     my($proto) = @_;
     return $proto->vs_call(
@@ -44,17 +50,14 @@ sub vs_learn_more {
     return shift->vs_call('Link', '[learn more]', shift, 'learn_more');
 }
 
-sub vs_base_menu {
-    my($proto, $values) = @_;
-    return $proto->vs_call(
-	Join => [map($proto->vs_link(@$_), @$values)],
-	{
-	    join_separator => $proto->vs_call(
-		Image => heart_9 => '', {class => 'sep'},
-	    ),
-	}
-    );
+sub vs_main_img {
+    my(undef, $img) = @_;
+    return Tag(div => '', "index_$img", {tag_if_empty => 1});
 }
 
+sub vs_prose {
+    my(undef, $prose) = @_;
+    return Tag(div => Prose($prose), 'prose');
+}
 
 1;
