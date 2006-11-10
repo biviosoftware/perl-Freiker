2f927162a1f087ca8eab6744660b3a73885d02d2
Rob Nagler <github@q33.us>
2006-11-10 06:24:54 +0000

    *** empty log message ***

---
+++ Model/YearBaseList.pm
@@ -0,0 +1,20 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::YearBaseList;
+use strict;
+use base 'Bivio::Biz::ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_prepare_statement {
+    my($self, $stmt) = @_;
+    $stmt->where(
+	$stmt->GTE(
+	    'Ride.ride_date', [Freiker::Biz->current_school_year_start_date]),
+	$stmt->LTE(
+	    'Ride.ride_date', [Freiker::Biz->current_school_year_end_date]),
+    );
+    return shift->SUPER::internal_prepare_statement(@_);
+}
+
+1;
