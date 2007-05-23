f675b260c3859ebd84598215aacea89eaa676a62
Rob Nagler <github@q33.us>
2007-05-23 03:54:16 +0000

    generate_image

--- Test/Freiker.pm
+++ Test/Freiker.pm
@@ -5,11 +5,21 @@ use strict;
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 use base ('Bivio::Test::Language::HTTP');
 use Freiker::Test;
+use File::Temp ();
 
 sub do_logout {
     my($self) = @_;
     $self->follow_link(qr{logout}i);
     return;
+}
+
+sub generate_image {
+    my($self, $text) = @_;
+    my(undef, $file) = $self->tmp_file('image.jpg');
+    $text =~ s/"/'/g;
+    $text =~ s/\S+/ /sg;
+    system(qq{convert -size 150x150 xc:white -fill blue -pointsize 36 -draw 'text 0,100 "$text"' $file}) == 0 || die;
+    return $file;
 }
 
 sub login_as {
