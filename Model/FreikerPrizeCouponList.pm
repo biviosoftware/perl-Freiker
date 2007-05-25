32eceb6019339f067c2aac95e653fea46d36f85f
Rob Nagler <github@q33.us>
2007-05-25 11:06:52 +0000

    *** empty log message ***

--- Model/FreikerPrizeCouponList.pm
+++ Model/FreikerPrizeCouponList.pm
@@ -10,6 +10,17 @@ sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
+        parent_id => ['RealmUser.user_id'],
+	auth_id => 'RealmUser.realm_id',
+	primary_key => [
+	    [qw(PrizeCoupon.realm_id PrizeReceipt.realm_id(+))],
+	    [qw(PrizeCoupon.coupon_code PrizeReceipt.coupon_code(+))],
+	],
+	other => [
+	    ['RealmUser.role', ['MEMBER']],
+	    [qw(PrizeCoupon.prize_id Prize.prize_id)],
+	    'Prize.name',
+	],
     });
 }
 
