b5f686beecddcf3a68f5052bd815a1317ff38b31
Rob Nagler <github@q33.us>
2007-08-20 03:02:44 +0000

    ignore years for now

--- Model/YearBaseList.pm
+++ Model/YearBaseList.pm
@@ -11,15 +11,15 @@ sub find_row_by_date {
     return shift->find_row_by('Ride.ride_date', shift);
 }
 
-sub internal_prepare_statement {
-    my($self, $stmt) = @_;
-    $stmt->where(
-	$stmt->GTE(
-	    'Ride.ride_date', [Freiker::Biz->current_school_year_start_date]),
-	$stmt->LTE(
-	    'Ride.ride_date', [Freiker::Biz->current_school_year_end_date]),
-    );
-    return shift->SUPER::internal_prepare_statement(@_);
-}
+# sub internal_prepare_statement {
+#     my($self, $stmt) = @_;
+#     $stmt->where(
+# 	$stmt->GTE(
+# 	    'Ride.ride_date', [Freiker::Biz->current_school_year_start_date]),
+# 	$stmt->LTE(
+# 	    'Ride.ride_date', [Freiker::Biz->current_school_year_end_date]),
+#     );
+#     return shift->SUPER::internal_prepare_statement(@_);
+# }
 
 1;
