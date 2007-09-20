7900356225b576cc1de58f435ad27615e4532468
Rob Nagler <github@q33.us>
2007-09-20 00:04:12 +0000

    moved to wiki.css

--- View/CSS.pm
+++ View/CSS.pm
@@ -42,14 +42,19 @@ td.main_left div.login input {
   margin: auto;
   display: block;
 }
-td.main_left div.task_menu a {
+td.main_left div.task_menu a,
+td.main_left div.left_nav a {
   background-image: none;
   display: list-item;
   padding: 0;
   margin: 0;
   list-style: none outside Icon('left_menu_sep');;
   margin-left: 20px;
-  Font('header_menu');
+  Font('nav');
+}
+td.main_left div.task_menu a:hover,
+td.main_left div.left_nav a:hover {
+  Font('a_hover');
 }
 td.main_middle {
   padding-left: 1em;
@@ -64,15 +69,6 @@ td.footer_middle div.task_menu {
 a img, a:hover img {
   border: 1px solid;
   Color('a_img-border');
-}
-.header_middle .task_menu {
-  text-transform: lowercase;
-  border-bottom: 1px solid;
-  Color('header_menu-border-bottom');
-  Font('header_menu');
-}
-.header_middle .nav a {
-  white-space: nowrap;
 }
 a:hover img {
   Color('a_hover_img-border');
@@ -91,101 +87,10 @@ div.topic {
   text-transform: none;
   Color('notice');
 }
-a.movie {
-  display: block;
-  float: left;
-  margin: 0 1em 1em 0;
-}
-img.graph {
-  display: block;
-  float: right;
-  margin: 1em 0 1em 1em;
-}
-div.wiki {
-  width: 40em;
-}
-.prose, .form_prose, .list_prose, div.press, table.prizes {
+.form_prose, .list_prose, table.prizes {
   width: 40em;
   text-align: left;
   padding: 1ex 0 1ex 0;
-}
-.prose, div.press {
-  padding: 0;
-}
-.press dt {
-  margin-top: 1.5ex;
-}
-.press dd {
-  margin: .3ex 0 0 0;
-}
-.press h3 {
-  margin-top: 2ex;
-  margin-bottom: 2ex;
-}
-.press h4 {
-  margin-top: 1ex;
-  margin-bottom: 0;
-}
-.press div {
-  margin-top: 1.5ex;
-}
-.press p {
-  margin: 1.5ex 0 1.5ex 0;
-  text-align: left;
-}
-.index_freiker, .index_wheel {
-  height: 200px;
-}
-.index_freiker {
-  float: left;
-  clear: left;
-  padding-right: 20px;
-  background: Icon('index_freiker'); no-repeat;
-  width: 95px;
-}
-.index_wheel {
-  clear: right;
-  margin-left: 20px;
-  float: right;
-  background: Icon('index_wheel'); no-repeat;
-  width: 180px;
-}
-.index_winners {
-  display: block;
-  margin: auto;
-  background: Icon('index_winners');no-repeat;
-  width: 250px;
-  height: 212px;
-}
-div.crest_view {
-  margin: 1ex 0 1ex 0;
-}
-#gears div {
-  text-align: center;
-}
-#gears .simple img {
-  padding: 10px;
-  vertical-align: middle;
-}
-.learn_more {
-  font-size: 80%;
-  white-space: nowrap;
-}
-.prize_rides {
-  font-weight: bold;
-}
-.prize_left {
-  padding-right: 2em;
-  clear: left;
-  float: left;
-}
-.prize_right {
-  padding-left: 2em;
-  clear: right;
-  float: right;
-}
-.prize_winners {
-  padding-top: 20px;
 }
 .list .label {
   padding-top: .2em;
@@ -206,63 +111,8 @@ form table.simple {
 form .list th, form .list td {
   padding: 0 .5em 0 .5em;
 }
-.note {
-  font-weight: bold;
-  padding: .2em;
-  margin-bottom: 1ex;
-  border: 1px solid red;
-  text-align: center;
-}
-.line {
-  width: 40em;
-  border: 0;
-  border-top: 1px solid;
-  Color('line-border-top');
-  height: 2px;
-  text-align: right;
-}
-.release .label {
-  padding-top: 2ex;
-  padding-bottom: 2ex;
-  text-align: left;
-}
 table.radio_grid {
   margin-left: 0;
-}
-div.donate {
-  margin: 2ex 0 2ex 0;
-  text-align: center;
-}
-div.donate div.msg {
-  text-align: center;
-  font-size: 120%;
-  font-weight: bold;
-  font-style: italic;
-  Color('notice');
-  text-decoration: underline;
-}
-.money {
-  font-size: 120%;
-  font-weight: bold;
-  Color('notice');
-  margin-right: .5em;
-}
-.donate form div.field_err {
-  display: inline;
-  margin-right: .5em;
-  font-weight: bold;
-  font-size: 100%;
-}
-.thanks {
-  text-align: center;
-  padding: .5ex;
-  font-size: 120%;
-  font-weight: bold;
-  Color('notice');
-}
-.address {
-  font-size: 120%;
-  font-weight: bold;
 }
 table.prizes tr.even {
   Color('prizes-background');
@@ -286,12 +136,6 @@ table.prizes span.desc p.prose {
 }
 table.prizes span.name, table.prizes span.rides {
   Font('strong');
-}
-body.body_wiki_Home .main_top .title {
-  display: none;
-}
-body.body_wiki_Home .main_top .byline {
- float: none;
 }
 EOF
 
