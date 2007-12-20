7c453b45c3f05b4d96a6a67351dea54de7598c8f
Rob Nagler <github@q33.us>
2007-12-20 00:21:20 +0000

    seems to be working

--- Util/SQL.pm
+++ Util/SQL.pm
@@ -122,4 +122,211 @@ sub initialize_test_data {
     return;
 }
 
+sub internal_upgrade_db_freiker_code {
+    my($self) = @_;
+    my($req) = $self->req;
+    my($rides) = do('Ride.pl') || die($@);
+    my($freiker_codes) = do('FreikerCode.pl') || die($@);
+    my($uploads) = [];
+    my($code_to_user) = {map(
+	($_->{freiker_code} => $_->{realm_id}),
+	grep($_->{realm_id} =~ /001/, @$rides),
+    )};
+    my($code_to_club) = {map(
+	($_->{freiker_code} => $_->{club_id}),
+	@$freiker_codes,
+    )};
+    my($realm_users) = {};
+    $self->model('RealmUser')->do_iterate(
+	sub {
+	    my($copy) = shift->get_shallow_copy;
+	    push(@{$realm_users->{$copy->{user_id}} ||= []}, $copy);
+	    return 1;
+	},
+	'unauth_iterate_start',
+	'creation_date_time desc',
+	{role => Bivio::Auth::Role->MEMBER},
+    );
+    my($freikometers) = {};
+    $self->model('RealmUser')->do_iterate(
+	sub {
+	    my($r, $u) = shift->get(qw(realm_id user_id));
+	    push(@{$freikometers->{$r} ||= []}, $u);
+	    return 1;
+	},
+	'unauth_iterate_start',
+	'creation_date_time desc',
+	{role => Bivio::Auth::Role->FREIKOMETER},
+    );
+    my($old_epc) = $self->use('Type.OldEPC');
+    my($user) = $self->model('User');
+    my($now) = $self->use('Type.DateTime')->now;
+    my($auth_id) = '';
+    my($zip);
+    my($exists) = {};
+    foreach my $fc (sort {$a->{club_id} cmp $b->{club_id}} @$freiker_codes) {
+	unless ($auth_id eq $fc->{club_id}) {
+	    $req->set_realm($auth_id = $fc->{club_id});
+	    $zip = $self->model('Address')->load->get('zip');
+	}
+	my($code) = $fc->{freiker_code};
+	$fc->{epc} = $old_epc->new($zip, $code)->as_string;
+	if ($fc->{user_id} = $code_to_user->{$code}) {
+	    my($ru) = $self->model('RealmUser');
+	    foreach my $copy (@{$realm_users->{$fc->{user_id}}}) {
+		next if $exists->{$copy->{user_id} . ' ' . $copy->{realm_id}}++;
+		$ru->unauth_delete($copy);
+		$ru->create({
+		    %$copy,
+		    role => Bivio::Auth::Role->FREIKER,
+		});
+		$fc->{modified_date_time} = $copy->{creation_date_time};
+	    }
+	}
+	else {
+	    $code_to_user->{$code}
+		= $fc->{user_id} = $user->create_freiker($code);
+	    $fc->{modified_date_time} = $now;
+	}
+    }
+    my($ride_uploads) = {};
+    $exists = {};
+    foreach my $r (sort {$a->{creation_date_time} cmp $b->{creation_date_time}} @$rides) {
+	$r->{ride_upload_id} = $ride_uploads->{$r->{creation_date_time}} ||= {
+	    club_id => $code_to_club->{$r->{freiker_code}} || die('1'),
+	    creation_date_time => $r->{creation_date_time} || die('2'),
+	    freikometer_user_id => $freikometers->{$code_to_club->{$r->{freiker_code}}}->[0] || die('3'),
+	} unless $r->{is_manual_entry};
+	$r->{user_id} = $code_to_user->{$r->{freiker_code}}
+	    || die($r->{freiker_code});
+	Bivio::IO::Alert->info($r->{user_id} . ' ' . $r->{ride_date} . ' ' . $r->{freiker_code})
+	    if $exists->{$r->{user_id} . ' ' . $r->{ride_date}}++;
+    }
+    $self->run(<<'EOF');
+DROP TABLE ride_t
+/
+DROP TABLE freiker_code_t
+/
+EOF
+    $self->run(<<'EOF');
+CREATE TABLE freiker_code_t (
+  club_id NUMERIC(18) NOT NULL,
+  freiker_code NUMERIC(9) NOT NULL,
+  epc CHAR(24) NOT NULL,
+  user_id NUMERIC(18) NOT NULL,
+  modified_date_time DATE NOT NULL,
+  CONSTRAINT freiker_code_t1 PRIMARY KEY(freiker_code)
+)
+/
+CREATE TABLE ride_t (
+  user_id NUMERIC(18) NOT NULL,
+  ride_date DATE NOT NULL,
+  ride_time DATE NOT NULL,
+  ride_upload_id NUMERIC(18),
+  CONSTRAINT ride_t1 PRIMARY KEY(user_id, ride_date)
+)
+/
+
+CREATE TABLE ride_upload_t (
+  ride_upload_id NUMERIC(18) NOT NULL,
+  club_id NUMERIC(18) NOT NULL,
+  creation_date_time DATE NOT NULL,
+  freikometer_user_id NUMERIC(18) NOT NULL,
+  CONSTRAINT ride_upload_t1 PRIMARY KEY(ride_upload_id)
+)
+/
+CREATE SEQUENCE ride_upload_s
+  MINVALUE 100022
+  CACHE 1 INCREMENT BY 100000
+/
+CREATE INDEX freiker_code_t2 ON freiker_code_t (
+  club_id
+)
+/
+ALTER TABLE freiker_code_t
+  ADD CONSTRAINT freiker_code_t3
+  FOREIGN KEY (club_id)
+  REFERENCES club_t(club_id)
+/
+CREATE INDEX freiker_code_t4 ON freiker_code_t (
+  user_id
+)
+/
+ALTER TABLE freiker_code_t
+  ADD CONSTRAINT freiker_code_t5
+  FOREIGN KEY (user_id)
+  REFERENCES user_t(user_id)
+/
+CREATE UNIQUE INDEX freiker_code_t6 ON freiker_code_t (
+  epc
+)
+/
+CREATE INDEX freiker_code_t7 ON freiker_code_t (
+  modified_date_time
+)
+/
+ALTER TABLE ride_t
+  ADD CONSTRAINT ride_t2
+  FOREIGN KEY (user_id)
+  REFERENCES user_t(user_id)
+/
+CREATE INDEX ride_t3 ON ride_t (
+  user_id
+)
+/
+CREATE INDEX ride_t4 ON ride_t (
+  ride_date
+)
+/
+ALTER TABLE ride_t
+  ADD CONSTRAINT ride_t5
+  FOREIGN KEY (ride_upload_id)
+  REFERENCES ride_upload_t(ride_upload_id)
+/
+CREATE INDEX ride_t6 ON ride_t (
+  ride_upload_id
+)
+/
+
+--
+-- ride_upload_id
+--
+ALTER TABLE ride_upload_t
+  ADD CONSTRAINT ride_upload_t2
+  FOREIGN KEY (club_id)
+  REFERENCES club_t(club_id)
+/
+CREATE INDEX ride_upload_t3 ON ride_upload_t (
+  club_id
+)
+/
+ALTER TABLE ride_upload_t
+  ADD CONSTRAINT ride_upload_t4
+  FOREIGN KEY (freikometer_user_id)
+  REFERENCES user_t(user_id)
+/
+CREATE INDEX ride_upload_t5 ON ride_upload_t (
+  freikometer_user_id
+)
+/
+CREATE INDEX ride_upload_t4 ON ride_upload_t (
+  creation_date_time
+)
+/
+EOF
+    foreach my $r (values(%$ride_uploads)) {
+	$r->{ride_upload_id} = $self->model('RideUpload')->create($r)
+	    ->get('ride_upload_id');
+    }
+    foreach my $fc (@$freiker_codes) {
+	$self->model('FreikerCode')->create($fc);
+    }
+    foreach my $r (@$rides) {
+	$r->{ride_upload_id} = $r->{ride_upload_id}->{ride_upload_id} || die
+	    if $r->{ride_upload_id};
+	$self->model('Ride')->create($r);
+    }
+    return;
+}
+
 1;
