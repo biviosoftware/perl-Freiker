aa1996dba798d872746ad7d89d9ab0cdb3608b68
Rob Nagler <github@q33.us>
2008-05-13 08:10:54 +0000

    working

--- Model/ClubPrizeForm.pm
+++ Model/ClubPrizeForm.pm
@@ -6,6 +6,24 @@ use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub execute_empty {
+    my($self) = @_;
+    $self->internal_put_field(
+	'PrizeRideCount.ride_count' =>
+	    $self->req(qw(Model.ClubPrizeList PrizeRideCount.ride_count)));
+    return;
+}
+
+sub execute_ok {
+    my($self) = @_;
+    $self->new_other('PrizeRideCount')->create_or_update({
+	prize_id =>
+	    $self->req(qw(Model.ClubPrizeList Prize.prize_id)),
+	ride_count => $self->get('PrizeRideCount.ride_count'),
+    });
+    return;
+}
+
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
--- Model/ClubPrizeList.pm
+++ Model/ClubPrizeList.pm
@@ -6,4 +6,18 @@ use Bivio::Base 'Model.AvailablePrizeList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	order_by => [
+	    'PrizeRideCount.ride_count',
+	],
+	other => [
+	    [qw(Prize.prize_id PrizeRideCount.prize_id)],
+	],
+        auth_id => 'PrizeRideCount.realm_id'
+    });
+}
+
 1;
