5dc246e3ee8672dc5493a3dc265e3ee9c3586347
Rob Nagler <github@q33.us>
2008-08-08 14:44:56 +0000

    added X-FreikometerDownload header

--- Action/FreikometerDownload.pm
+++ Action/FreikometerDownload.pm
@@ -7,24 +7,28 @@ use Bivio::Base 'Action.RealmFile';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_FOLDER_RE) = qr{^\Q@{[
     Bivio::Biz::Model->get_instance('FreikometerDownloadList')->FOLDER
-]}/}o;
+]}/}io;
+my($_FU) = b_use('Action.FreikometerUpload');
+my($_M) = b_use('Biz.Model');
+
 sub execute_get {
     my($proto, $req, @rest) = @_;
     my(@res) = $proto->SUPER::access_controlled_execute($req, @rest);
-    my($rf) = Bivio::Biz::Model->new($req, 'RealmFile');
-    my($pi) = $req->get('path_info');
+    my($rf) = $req->get('Model.RealmFile');
     $req->throw_die(CORRUPT_QUERY => {
 	message => 'not in download folder',
-	entity => $pi,
-    }) unless $pi =~ $_FOLDER_RE;
-    $rf->delete({path => $pi});
+	entity => $rf
+    }) unless $rf->get('path') =~ $_FOLDER_RE;
+    $_FU->reply_header_out(
+	'X-FreikometerDownload', $rf->get_content_length, $req);
+    $rf->delete;
     return @res;
 }
 
 sub execute_redirect_next_get {
     my($proto, $req) = @_;
-    my($l) = Bivio::Biz::Model->new($req, 'FreikometerDownloadList')
-        ->load_page({count => 1});
+    my($l) = $_M->new($req, 'FreikometerDownloadList')->load_page({count => 1});
+    $_FU->reply_header_out('X-FreikometerDownload', -1, $req);
     return {
 	method => 'client_redirect',
 	task_id => 'BOT_FREIKOMETER_DOWNLOAD',
--- Action/FreikometerUpload.pm
+++ Action/FreikometerUpload.pm
@@ -2,22 +2,23 @@
 # $Id$
 package Freiker::Action::FreikometerUpload;
 use strict;
-use Bivio::Base 'Bivio::Biz::Action';
-use Bivio::IO::Log;
+use Bivio::Base 'Biz.Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_DT) = __PACKAGE__->use('Type.DateTime');
-my($_FP) = __PACKAGE__->use('Type.FilePath');
-my($_D) = __PACKAGE__->use('Type.Date');
-my($_RF) = __PACKAGE__->use('Action.RealmFile');
+my($_DT) = b_use('Type.DateTime');
+my($_FP) = b_use('Type.FilePath');
+my($_D) = b_use('Type.Date');
+my($_RF) = b_use('Action.RealmFile');
+my($_L) = b_use('IO.Log');
+my($_M) = b_use('Biz.Model');
 
 sub execute {
     my($proto, $req) = @_;
     return
 	unless $req->is_http_method('put');
-    Bivio::Biz::Model->new($req, 'RealmUser')->set_realm_for_freikometer;
+    $_M->new($req, 'RealmUser')->set_realm_for_freikometer;
     my($pi) = $_FP->get_clean_tail($req->get('path_info'));
-    Bivio::IO::Log->write_compressed(
+    $_L->write_compressed(
 	File::Spec->catfile(
 	    $req->get('auth_user')->get('name'),
 	    $_DT->local_now_as_file_name . '-' . $pi,
@@ -25,11 +26,20 @@ sub execute {
 	$req->get_content,
 	$req,
     );
-    Bivio::Biz::Model->new($req, 'RideImportForm')->process_content
+    $_M->new($req, 'RideImportForm')->process_content
         if $pi =~ /\.csv$/;
     $req->set_realm($req->get('auth_user'));
     $req->put(path_info => "/upload/$pi");
-    return $_RF->execute_put($req);
+    $_RF->execute_put($req);
+    $proto->reply_header_out(
+	'X-FreikometerUpload', length(${$req->get_content}), $req);
+    return;
+}
+
+sub reply_header_out {
+    my(undef, $key, $value, $req) = @_;
+    $req->get('r')->header_out($key, $value);
+    return;
 }
 
 1;
