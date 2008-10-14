e908c73b871c1807b489e6e618d9a4e2ce9bb1cb
Rob Nagler <github@q33.us>
2008-10-14 21:50:44 +0000

    *** empty log message ***

---
+++ Version.pm
@@ -0,0 +1,43 @@
+# Copyright (c) 2005-2008 bivio Software, Inc.  All Rights reserved.
+# $Id$
+package Freiker::Version;
+use strict;
+use Bivio::Base 'Bivio::UNIVERSAL';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+=head1 NAME
+
+Freiker::Version - Freiker application overview and version
+
+=head1 RELEASE SCOPE
+
+Freiker
+
+=head1 SYNOPSIS
+
+    use Freiker::Version;
+
+=head1 DESCRIPTION
+
+C<Freiker> is a high-tech bicycle communting encouragement program.  The
+software supports automated ride-counting, registration, prize selection,
+sponsorships, etc.
+
+Please visit http://www.freiker.org for more info.
+
+=head1 CHANGES
+
+  $Log$
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005-2008 bivio Software, Inc.  All Rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
