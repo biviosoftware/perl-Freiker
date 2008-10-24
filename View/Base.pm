41c7d3c207b0c8a1cc26fe954b2177a9d50a4c84
Dave Taenzer <dtaenzer@bivio.biz>
2008-10-24 22:38:43 +0000

    new web site

--- View/Base.pm
+++ View/Base.pm
@@ -6,189 +6,86 @@ use Bivio::Base 'View.ThreePartPage';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DT) = b_use('Type.DateTime');
 
 sub VIEW_SHORTCUTS {
     return 'Freiker::ViewShortcuts';
 }
 
 sub internal_xhtml_adorned {
-    my($self) = @_;
-    my(@res) = shift->SUPER::internal_xhtml_adorned(@_);
-    view_unsafe_put(
-	xhtml_title => Join([
-	    If([qw(auth_realm type ->eq_club)],
-	       Join([
-		   String([qw(auth_realm owner display_name)]),
-		   ': ',
-	       ]),
-	    ),
-	    Prose(vs_text(
-		[sub {"xhtml.title.$_[1]"}, ['task_id', '->get_name']])),
-	]),
-	wiki_widget_contact => Link(
-	    String('our contact form'),
-	    'GENERAL_CONTACT',
-	),
-	wiki_widget_paypal_form => DIV_donate(
-	    Form(PayPalForm => Join([
-		SPAN_money('$'),
-		FormField('PayPalForm.amount', {class => 'money'}),
-		FormButton('ok_button'),
-	    ])),
-	),
-	xhtml_dock_left => If(
-	    And(
-		['auth_realm', 'type', '->eq_forum'],
-	    ),
-	    TaskMenu([
-		'FORUM_BLOG_LIST',
-		'FORUM_CALENDAR',
-		{
-		    task_id => 'FORUM_FILE',
-		    control => ['->can_user_execute_task', 'FORUM_FILE_CHANGE'],
-		},
-		'FORUM_MAIL_THREAD_ROOT_LIST',
-		'FORUM_MOTION_LIST',
-		'FORUM_TUPLE_USE_LIST',
-		'FORUM_WIKI_VIEW',
-	    ]),
-	),
-    );
-    view_unsafe_put(
-	xhtml_main_right => '',
-	xhtml_main_left => Join([
-	    If(['auth_user'],
-# List of Family, Schools, and Stores
-	       [sub {
-		    my($req) = shift->get_request;
-#TODO: Refactor: Make a redirect task for realms?
-		    TaskMenu([
-			map(+{
-			    task_id => $_,,
-			    realm => vs_constant('site_adm_realm_name'),
-			}, qw(SITE_ADM_USER_LIST SITE_ADM_SUBSTITUTE_USER)),
-			'ADM_FREIKOMETER_LIST',
-			'ADM_PRIZE_LIST',
-			'ADM_PRIZE_COUPON_LIST',
-			'FAMILY_FREIKER_LIST',
-			map(+{
-			    task_id => 'CLUB_FREIKER_LIST',
-			    label => String($_->{'RealmOwner.display_name'}),
-			    realm => $_->{'RealmOwner.name'},
-			    query => undef,
-			}, sort {
-			    $a->{'RealmOwner.display_name'}
-				cmp $b->{'RealmOwner.display_name'}
-			} @{$req->map_user_realms(
-			    undef,
-			    {
-				'RealmOwner.realm_type' => Bivio::Auth::RealmType->CLUB,
-				'RealmUser.role' => Bivio::Auth::Role->ADMINISTRATOR,
-			    },
-			)}),
-			map(+{
-			    task_id => 'MERCHANT_PRIZE_LIST',
-			    label => String($_->{'RealmOwner.display_name'}),
-			    realm => $_->{'RealmOwner.name'},
-			    query => undef,
-			}, sort {
-			    $a->{'RealmOwner.display_name'}
-				cmp $b->{'RealmOwner.display_name'}
-			} @{$req->map_user_realms(
-			    undef,
-			    {
-				'RealmOwner.realm_type' => Bivio::Auth::RealmType->MERCHANT,
-			    },
-			)}),
-# 			map(+{
-# 			    task_id => 'MERCHANT_PRIZE_LIST',
-# 			    label => String($_->{'RealmOwner.display_name'}),
-# 			    realm => $_->{'RealmOwner.name'},
-# 			    query => undef,
-# 			}, sort {
-# 			    $a->{'RealmOwner.display_name'}
-# 				cmp $b->{'RealmOwner.display_name'}
-# 			} @{$req->map_user_realms(
-# 			    undef,
-# 			    {
-# 				'RealmOwner.realm_type' => Bivio::Auth::RealmType->FORUM,
-# 			    },
-# 			)}),
-			{
-			    task_id => 'CLUB_REGISTER',
-			    control => [['->req'], '->is_test'],
-			},
-# 			'MERCHANT_REGISTER',
-		    ]),
-		}],
-		If([sub {
-			my($req) = shift->get_request;
-			return 0
-			    if $req->get('task_id')->get_name =~ /LOGIN|USER_CREATE/;
-			Bivio::Biz::Model->new($req, 'ContextlessUserLoginForm')->process;
-			return 1;
-		   }],
+    return shift->call_super_before(\@_, sub {
+	view_unsafe_put(
+	    xhtml_title => Join([
+		If([qw(auth_realm type ->eq_club)],
 		   Join([
-		       DIV_login(Join([RoundedBox(
-			   Form(ContextlessUserLoginForm =>
-			       Join([
-				   map((
-				       DIV_label(String(vs_text("ContextlessUserLoginForm.$_"))),
-				       FormField("ContextlessUserLoginForm.$_", {size => 15}),
-				   ), qw(login RealmOwner.password)),
-				   StandardSubmit('ok_button'),
-				   Link('Not Registered?', 'USER_CREATE', {
-				       class => 'label',
-				   }),
-			       ]),
-			       {
-				   action => URI({
-				       task_id => 'LOGIN',
-				       no_context => 1,
-				   }),
-			       },
-			    ),
-		       ), DIV_sandbag1('', {tag_if_empty => 1})])),
-		       DIV_sandbag2('', {tag_if_empty => 1}),
+		       String([qw(auth_realm owner display_name)]),
+		       ': ',
 		   ]),
-		   ' ',
-	       ),
-	    ),
-	    WikiText('@b-menu.left_nav Main', {
-		realm_id => vs_constant('site_realm_id'),
-		realm_name => vs_constant('site_realm_name'),
-		task_id => 'FORUM_WIKI_VIEW',
-	    }),
-	]),
-	xhtml_header_middle => DIV_donate(
-	    Link(
-		Join([
-		    DIV('Please Donate'),
-		    DIV_subtitle('even $10 helps!'),
-		]),
-		URI({
-		    realm => vs_constant('site_realm_name'),
-		    task_id => 'PAYPAL_FORM',
-		    query => undef,
-		}),
-	    ),
-	),
-	xhtml_footer_middle => Join([
-	    TaskMenu([
-		'GENERAL_CONTACT',
-		{
-		    task_id => 'PAYPAL_FORM',
-		    realm => vs_constant('site_realm_name'),
-		    query => undef,
-		},
-		'SITE_ROOT',
-		XLink('user_logged_out'),
-		XLink('user_just_visitor'),
+		),
+		Prose(vs_text(
+		    [sub {"xhtml.title.$_[1]"}, ['task_id', '->get_name']])),
 	    ]),
-	    DIV_tag_line('Saving our society, one bike ride at a time.&#153;'),
-	]),
-    );
-    return @res;
+	    wiki_widget_contact => Link(
+		String('our contact form'),
+		'GENERAL_CONTACT',
+	    ),
+	    wiki_widget_paypal_form => DIV_donate(
+		Form(PayPalForm => Join([
+		    SPAN_money('$'),
+		    FormField('PayPalForm.amount', {class => 'money'}),
+		    FormButton('ok_button'),
+		])),
+	    ),
+	    xhtml_dock_left => If(
+		And(
+		    ['auth_realm', 'type', '->eq_forum'],
+		),
+		TaskMenu([
+		    'FORUM_BLOG_LIST',
+		    'FORUM_CALENDAR',
+		    {
+			task_id => 'FORUM_FILE',
+			control => ['->can_user_execute_task', 'FORUM_FILE_CHANGE'],
+		    },
+		    'FORUM_MAIL_THREAD_ROOT_LIST',
+		    'FORUM_MOTION_LIST',
+		    'FORUM_TUPLE_USE_LIST',
+		    'FORUM_WIKI_VIEW',
+		]),
+	    ),
+	);
+	view_unsafe_put(
+	    xhtml_header_middle => _menu('HeaderMiddle'),
+	    xhtml_footer_left => '',
+	    xhtml_footer_right => '',
+	    xhtml_footer_middle => Join([
+		_menu('FooterMiddle'),
+		DIV_legal(Join([
+		    SPAN_copyright(Join([
+			'&copy; ',
+			$_DT->now_as_year,
+			' ',
+			vs_text_as_prose('site_copyright'),
+		    ])),
+		    SPAN_tagline('Every Ride Counts!&trade;'),
+		    Link(
+			'Software by bivio',
+			'http://www.bivio.biz',
+			{class => 'software_by_bivio'},
+		    ),
+		])),
+	    ]),
+	);
+	return;
+    });
+}
+
+sub _menu {
+    my($name) = @_;
+    return WikiText('@b-menu ' . $name, {
+	realm_id => vs_constant('site_realm_id'),
+	task_id => 'SITE_WIKI_VIEW',
+    });
 }
 
 1;
--- View/CSS.pm
+++ View/CSS.pm
@@ -7,202 +7,300 @@ use Bivio::Base 'Bivio::UI::View::CSS';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 my($_SITE) = <<'EOF';
-!TODO: Remove after 1/15/2007
-td.header_left .logo_su a.logo:hover {
-  text-decoration: none;
-}
+/***** from common.css *****/
 body {
-  width: 60em;
-  padding-left: 1em;
-  padding-right: 1em;
+  background-color:#aaa; /* TMP */
+  background-color:#45B00C;
+  font-family:Verdana, Arial, Helvetica, sans-serif;
+  font-size: .9em;
+  width: 77em;
   margin-left: auto;
   margin-right: auto;
   margin-top: 0;
 }
-h2 {
-  Font('title');
+
+/***** dock styles *****/
+table.dock a {
+  color:#FFFFFF;
 }
-.main_top .byline {
-  float: right;
+
+/***** header styles *****/
+table.header {
+  width: 100%;
 }
-# td.header_left, td.header_right, td.main_left {
-#   width: 11em;
-# }
-td.header_left {
-  background-position: left center;
-}
-td.header_middle {
-  width: auto;
-  padding-top: 2ex;
-}
-td.header_middle div.donate a div {
-  text-align: center;
-  margin: auto;
-}
-td.header_middle div.donate a div {
-  font-size: 200%;
-  font-weight: bold;
-  Color('a_link');
-}
-td.header_middle div.donate a:hover {
-  Font('a_hover');
-}
-td.header_middle div.donate a div.subtitle {
-  font-size: 80%;
-  font-style: italic;
-  text-decoration: underline;
-}
-td.main_left div.login div.rounded_box_body {
-  margin-bottom: 1ex;
-}
-td.main_left div.login {
-  text-align: center;
-  Color('left_login-background');
-  margin: auto;
-}
-td.main_left div.login div.sandbag1 {
-  height: 1px;
-}
-td.main_left div.sandbag2 {
-  height: .5em;
-}
-td.main_left div.login .label,
-td.main_left div.login .standard_submit {
-  padding-top: .5ex;
-  Color('main_left_text');
-  text-align: center;
-}
-td.main_left div.login a.label {
-  display: block;
-}
-td.main_left div.login input {
-  margin: auto;
-  display: block;
-}
-td.main_left div.task_menu a,
-td.main_left div.left_nav a {
-  background-image: none;
-  display: block;
-  margin: 0;
-  padding: .1em .2em;
-  Font('nav');
-}
-td.main_left div.task_menu a:hover,
-td.main_left div.left_nav a:hover {
-  Font('left_nav_a_hover');
-  Color('left_nav-background');
-  text-decoration: none;
-}
-td.main_middle {
-  padding-left: 1em;
-  padding-right: 1em;
-}
-div.main_body {
-  float: none;
-}
-td.footer_middle div.task_menu {
-  text-align: center;
-}
-td.footer_middle div.tag_line {
-  text-align: center;
-  margin-top: 2ex;
-  Font('footer_tag_line');
-}
-a img, a:hover img {
-  border: 1px solid;
-  Color('a_img-border');
-}
-a:hover img {
-  Color('a_hover_img-border');
-}
-div.topic {
-  Font('topic');
-  margin-bottom: 1ex;
-}
-.acknowledgement {
-  margin-left: 0;
-}
-.notice {
-  padding: .2em;
-  width: 14em;
-  text-align: right;
-  text-transform: none;
-  Color('notice');
-}
-.form_prose, .list_prose, table.prizes {
-  width: 40em;
-  text-align: left;
-  padding: 1ex 0 1ex 0;
-}
-! IE Bug: If you set .prose, it'll work badly
-.prose {
-  width: auto;
-}
-.prose a {
-  text-decoration: underline;
-}
-.list .label {
-  padding-top: .2em;
-  padding-right: .5em;
-  text-align: right;
-  width: 50%;
-}
-.list .value {
-  padding-top: .2em;
-  width: 50%;
-  padding-left: .5em;
-  text-align: left;
-}
-form table.simple {
-  text-align: left;
-  margin: 0;
-}
-form .list th, form .list td {
-  padding: 0 .5em 0 .5em;
-}
-table.radio_grid {
-  margin-left: 0;
-}
-table.prizes tr.even {
-  Color('prizes-background');
-}
-table.prizes tr.even img {
-  float: right;
-  margin-left: 1em;
-}
-table.prizes tr.odd img {
-  float: left;
-  margin-right: 1em;
-}
-table.prizes a:hover {
-  Color('a_link');
-}
-table.prizes a:hover img {
-  Color('prize_img-border');
-}
-table.prizes span.desc p.prose {
-  display: inline;
-}
-span.strong, table.prizes span.name, table.prizes span.rides {
-  Font('strong');
-}
-span.sui {
-  Color('header_su-background');
-  Font('header_su');
-}
-span.prize_name {
-  Font('prize_name');
-}
-td.select_prize {
-  background: Icon('select_prize'); center center no-repeat;
-  height: Icon(qw(select_prize height));px;
-  width: Icon(qw(select_prize width));px;
-  text-align: center;
+table.header td.header_left {
+  padding: .5em;
+  width: 303px;
   vertical-align: middle;
 }
-td.select_prize a {
-  Font('select_prize');
+table.header td.header_middle {
+  text-align: center;
+  padding: .5em;
 }
+table.header td.header_middle ul {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.0em;
+  font-weight: bold;
+  background-color: #FFE401;
+  text-align: center;
+  vertical-align: middle;
+  display: block;
+  list-style-type: none;
+  margin: 0;
+  padding: .4em 0;
+}
+table.header td.header_middle ul li {
+  display: inline;
+  padding: .5em .4em;
+}
+table.header td.header_middle img {
+  padding-bottom: 1em;
+}
+table.header td.header_middle li a {
+  color: #7A0913;
+  text-decoration: none;
+}
+table.header td.header_middle li a:hover {
+  color: #45B00C;
+  text-decoration: none;
+}
+
+/***** generic oval styles *****/
+.b1f, .b2f, .b3f, .b4f {font-size:1px; overflow:hidden; display:block;}
+span.b1f {height:1px; margin:0 5px;}
+span.b2f {height:1px; margin:0 3px;}
+span.b3f {height:1px; margin:0 2px;}
+span.b4f {height:2px; margin:0 1px;}
+
+/***** generic main table styles *****/
+div.white_oval span {
+   background: #FFFFFF;
+}
+div.yellow_oval span {
+   background:#FFE401; 
+}
+table.main {
+  background-color: #FFFFFF;
+}
+table.main .main_left, table.main .main_middle{
+   vertical-align:top;
+}
+
+/***** footer *****/
+table.footer {
+  border:0;
+}
+td.footer_left, td.footer_right {
+  width: 0;
+}
+table.footer div.bmenu {
+  text-align:center;
+  margin-bottom: .3em;
+}
+table.footer tr td div.bmenu span {
+   padding: 0 1em;
+}
+table.footer div.bmenu a {
+  color: #FFFFFF;
+  text-decoration:none;
+}
+table.footer div.bmenu a:hover {
+  color: #FCF67C;
+}
+table.footer div.legal {
+  color: #FFFFFF;
+  text-align:center;
+}
+table.footer div.legal span {
+  color: #FFFFFFF;
+  padding: 0 1em;
+}
+table.footer div.legal a {
+  color: #FFFFFF;
+  text-decoration:none;
+}
+table.footer div.legal a:hover {
+   color: #FCF67C;
+}
+
+/***** from index (home) page *****/
+div.light_green_oval {
+0  margin: 0 .5em 0 0;
+}
+div.light_green_oval span {
+  background:#4DBF0F;
+}
+div.login_box {
+  background-color: #4DBF0F;
+  padding: 1em;
+  text-align: center;
+}
+div.login_box label {
+  font-family: Arial, Helvetica, sans-serif;
+  color:#FFFFFF;
+  display:block;
+}
+div.login_box input {
+  margin-bottom: .5em;
+  font-size: 1.0em;
+}
+div.login_box img {
+  padding-bottom: .3em;
+  margin: 0;
+}
+div.login_box form {
+  margin: 0;
+  padding: 0;
+}
+div.register {
+  padding: 1em;
+  background-color: #FFF6A6;
+  border: 4px solid #FFB20E;
+  margin: .5em .5em .5em 0;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.1em;
+  text-align: center;
+}
+div.register a {
+  color: #2A6D06;
+  text-decoration:none;
+}
+div.dark_green_oval span {
+  background:#2A6D06;
+}
+div.green_box {
+  background-color: #2A6D06;
+  padding: .5em;
+  color:#FFFFFF;
+}
+div.green_box p, div.green_box strong, div.green_box li {
+  color:#FFFFFF;
+  font-size:1.0em;
+}
+div.green_box li p {
+  padding-top: 0;
+}
+div.green_box ul {
+  margin-top:.5em;
+}
+div.green_box a {
+  color: #FCF67C;
+  text-decoration:none;
+}
+div.green_box table td {
+  vertical-align: top;
+  padding: .5em;
+}
+div.rides_counted {
+  background-color: #FFA60E;
+  border: #FF5D0E solid 4px;
+  margin-top: 0;
+  margin-right: .5em;
+  padding: 1em .5em;
+  text-align: center;
+}
+div.rides_counted p {
+  width: 12em;
+  font-family:Arial, Helvetica, sans-serif;
+  font-size: 2.5em;
+  font-weight:bold;
+  text-align:center;
+  margin: .2em 0 .2em 0;
+  color: #FFFFFF;
+}
+div.support_freiker {
+  background-color: #026EE6;
+  border: #082A4E solid 4px;
+  margin-right: 1em;
+  font-family:Arial, Helvetica, sans-serif;
+  font-size: .9em;
+  color: #FFFFFF;
+  padding: .3em;
+  height=100%;
+}
+tr.rides_support {
+  vertical-align: top;
+}
+div.support_text {
+  padding-left: .7em;
+  padding-bottom: .4em;
+  padding-right: .5em;
+  color: #FFFFFF;
+  font-size: 1.1em;
+}
+div.support_link {
+  text-align: center;
+}
+div.testimonials {
+  font-family:Verdana, Arial, Helvetica, sans-serif;
+  font-size: 0.8em;
+  margin: .5em;
+}
+div.wiki div.testimonials h3 {
+  font-size: 1.4em;
+  padding-top: .5em;
+}
+div.wiki div.testimonials p {
+  padding-bottom: .5em;
+}
+
+/***** new *****/
+td.fr_h_leftnav {
+  width: 8em;
+  text-align: center;
+  padding: 1em;
+}
+td.fr_h_content {
+  padding: 0 1em 1em 0;
+}
+div.login_box form {
+  text-align: center;
+  margin:0;
+  font-size: 1.1em;
+}
+.fr_label {
+  font-family: Arial, Helvetica, sans-serif;
+  color:#FFFFFF;
+  display:block;
+  font-size: 1.1em;
+}
+div.green_box p, div.green_box a {
+  font-size: 1.1em;
+}
+
+td.main_left {
+  width: 15em;
+  text-align: center;
+  padding: 1em;
+}
+
+/***** internal pages *****/
+
+div.wiki p {
+  padding-top: .5em;
+}
+div.wiki img.fr_top_right {
+  float:right;
+  margin: 0 1em 1em 1em;
+  border: #000000 solid 5px;
+}
+
+div.wiki h1 {
+  font-size:1.6em;
+  color: #000;
+}
+div.wiki h2 {
+  font-size:1.2em;
+}
+div.wiki h3 {
+  font-size:1.0em;
+  color: #000;
+}
+
+^/bp/.* div.main_top div.title {
+  display:none;
+}
+
 EOF
 
 sub internal_site_css {
