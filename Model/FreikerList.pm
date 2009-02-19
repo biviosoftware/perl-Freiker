cf36ebfac10df23b296c15f8ac22327a43752b94
Rob Nagler <github@q33.us>
2009-02-19 18:48:07 +0000

    push up code into FreikerBaseList

--- Model/FreikerList.pm
+++ Model/FreikerList.pm
@@ -2,11 +2,10 @@
 # $Id$
 package Freiker::Model::FreikerList;
 use strict;
-use Bivio::Base 'Model.YearBaseList';
+use Bivio::Base 'Model.FreikerBaseList';
 use Freiker::Biz;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_SA) = __PACKAGE__->use('Type.StringArray');
 
 sub PRIZE_SELECT_LIST {
     return 'PrizeSelectList';
@@ -15,49 +14,10 @@ sub PRIZE_SELECT_LIST {
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
-        version => 1,
-	primary_key => [
-	    [qw(RealmUser.user_id RealmOwner.realm_id)],
-	],
         order_by => [
 	    'RealmOwner.display_name',
 	],
 	other => [
-	    {
-		name => 'parent_display_name',
-		type => 'DisplayName',
-		constraint => 'NONE',
-		select_value => "(SELECT ro.display_name
-                    FROM realm_owner_t ro, realm_user_t ru
-                    WHERE ro.realm_type = @{[b_use('Auth.RealmType')->USER->as_sql_param]}
-                    AND ru.role = @{[b_use('Auth.Role')->FREIKER->as_sql_param]}
-                    AND ru.realm_id = ro.realm_id
-                    AND realm_user_t.user_id = ru.user_id
-                )",
-		sort_order => 0,
-	    },
-	    {
-		name => 'parent_email',
-		type => 'Email',
-		constraint => 'NONE',
-		select_value => "(SELECT e.email
-                    FROM realm_owner_t ro, realm_user_t ru, email_t e
-                    WHERE ro.realm_type = @{[b_use('Auth.RealmType')->USER->as_sql_param]}
-                    AND ru.role = @{[b_use('Auth.Role')->FREIKER->as_sql_param]}
-                    AND e.location = @{[b_use('Model.Email')->DEFAULT_LOCATION->as_sql_param]}
-                    AND ru.realm_id = ro.realm_id
-                    AND realm_user_t.user_id = ru.user_id
-                    AND e.realm_id = ro.realm_id
-                )",
-		sort_order => 0,
-	    },
-	    {
-		name => 'ride_count',
-		type => 'Integer',
-		constraint => 'NOT_NULL',
-		select_value => '(SELECT COUNT(*) FROM ride_t WHERE ride_t.user_id = realm_user_t.user_id)',
-		sort_order => 0,
-	    },
 	    {
 		name => 'prize_debit',
 		type => 'Integer',
@@ -75,15 +35,9 @@ sub internal_initialize {
 		type => 'Boolean',
 		constraint => 'NOT_NULL',
 	    },
-	    ['RealmUser.role', ['FREIKER']],
 	    {
 		name => 'prize_select_list',
 		type => 'Model.PrizeSelectList',
-		constraint => 'NOT_NULL',
-	    },
-	    {
-		name => 'freiker_codes',
-		type => 'StringArray',
 		constraint => 'NOT_NULL',
 	    },
 	],
@@ -96,8 +50,6 @@ sub internal_post_load_row {
     return 0
 	unless shift->SUPER::internal_post_load_row(@_);
     $row->{prize_credit} = $row->{ride_count} - ($row->{prize_debit} ||= 0);
-    $row->{freiker_codes} = $_SA->new($self->new_other('UserFreikerCodeList')
-	->get_codes($row->{'RealmUser.user_id'}));
     $row->{can_select_prize}
 	= ($row->{prize_select_list}
 	    = $self->new_other($self->PRIZE_SELECT_LIST)
@@ -106,24 +58,5 @@ sub internal_post_load_row {
 	)->get_result_set_size ? 1 : 0;
     return 1;
 }
-
-# sub internal_prepare_statement {
-#     my($self, $stmt) = @_;
-#     $stmt->from(
-# 	$stmt->LEFT_JOIN_ON(qw(RealmUser parent.RealmUser), [
-# 	    [qw(RealmUser.user_id parent.RealmUser.user_id)],
-# 	    ['parent.RealmUser.role', ['FREIKER']],
-# 	]),
-# 	$stmt->LEFT_JOIN_ON(qw(parent.RealmUser parent.RealmOwner), [
-# 	    [qw(parent.RealmUser.realm_id parent.RealmOwner.realm_id)],
-# 	    ['parent.RealmOwner.realm_type' => ['USER']],
-# 	]),
-# # 	$stmt->LEFT_JOIN_ON(qw(parent.RealmOwner parent.Email), [
-# # 	    [qw(parent.RealmOwner.realm_id parent.Email.realm_id)],
-# # 	    ['parent.Email.location' => [b_use('Model.Email')->DEFAULT_LOCATION]],
-# # 	]),
-#     );
-#     return shift->SUPER::internal_prepare_statement(@_);
-# }
 
 1;
