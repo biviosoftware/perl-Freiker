1fbed83181b3b5834660c49f68fa69e2354c8eaa
Rob Nagler <github@q33.us>
2009-04-06 17:48:18 +0000

    CLUB_RIDE_FILL_FORM factored out _tools()

--- View/Club.pm
+++ View/Club.pm
@@ -6,16 +6,9 @@ use Bivio::Base 'View.Base';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-
 sub ride_date_list {
     return shift->internal_put_base_attr(
-	tools => TaskMenu([qw(
-	    CLUB_FREIKER_LIST
-	    CLUB_FREIKER_CODE_IMPORT
-	    CLUB_PRIZE_LIST
-	    CLUB_PRIZE_COUPON_LIST
-	    GREEN_GEAR_LIST
-	)]),
+	_tools_rides('CLUB_RIDE_DATE_LIST'),
 	body => vs_paged_list(ClubRideDateList => [
 	    'Ride.ride_date',
 	    'ride_count',
@@ -25,14 +18,7 @@ sub ride_date_list {
 
 sub freiker_list {
     return shift->internal_put_base_attr(
-	tools => TaskMenu([qw(
-	    CLUB_FREIKER_SELECT
-	    CLUB_RIDE_DATE_LIST
-	    CLUB_FREIKER_CODE_IMPORT
-	    CLUB_PRIZE_LIST
-	    CLUB_PRIZE_COUPON_LIST
-	    GREEN_GEAR_LIST
-	)]),
+	_tools_rides('CLUB_FREIKER_LIST'),
 	body => vs_paged_list(ClubFreikerList => [
 	    'RealmOwner.display_name',
 	    'freiker_codes',
@@ -47,22 +33,20 @@ sub freiker_list {
 
 sub freiker_select {
     return shift->internal_put_base_attr(
+	_tools_prizes('CLUB_FREIKER_SELECT'),
 	body => vs_simple_form(FreikerSelectForm => [qw(
             FreikerSelectForm.FreikerCode.freiker_code
-	)]),
-	tools => TaskMenu([qw(
-	    CLUB_RIDE_DATE_LIST
-	    CLUB_FREIKER_CODE_IMPORT
-	    CLUB_PRIZE_LIST
-	    CLUB_PRIZE_COUPON_LIST
 	)]),
     );
 }
 
 sub manual_ride_form {
-    return shift->internal_body(vs_simple_form(ClubManualRideForm => [
-	'ClubManualRideForm.add_days',
-    ]));
+    return shift->internal_put_base_attr(
+	_tools_prizes('CLUB_FREIKER_MANUAL_RIDE_FORM'),
+	body => vs_simple_form(ClubManualRideForm => [
+	    'ClubManualRideForm.add_days',
+	]),
+    );
 }
 
 sub prize {
@@ -72,30 +56,39 @@ sub prize {
 }
 
 sub prize_confirm {
-    return shift->internal_body(vs_simple_form(ClubPrizeConfirmForm => [qw(
-        ClubPrizeConfirmForm.Prize.name
-	ClubPrizeConfirmForm.PrizeRideCount.ride_count
-    )]));
+    return shift->internal_put_base_attr(
+	_tools_prizes('CLUB_FREIKER_PRIZE_CONFIRM'),
+	body => vs_simple_form(ClubPrizeConfirmForm => [qw(
+	    ClubPrizeConfirmForm.Prize.name
+	    ClubPrizeConfirmForm.PrizeRideCount.ride_count
+	)]),
+    );
 }
 
 sub prize_coupon_list {
-     return shift->internal_body(vs_paged_list(ClubPrizeCouponList => [qw(
-	PrizeCoupon.creation_date_time
-	RealmOwner.display_name
-	family_display_name
-	freiker_codes
-	Prize.name
-     )]));
+    return shift->internal_put_base_attr(
+	_tools_prizes('CLUB_FREIKER_PRIZE_CONFIRM'),
+	body => vs_paged_list(ClubPrizeCouponList => [qw(
+	    PrizeCoupon.creation_date_time
+	    RealmOwner.display_name
+	    family_display_name
+	    freiker_codes
+	    Prize.name
+	)]),
+    );
 }
 
 sub prize_list {
-     return shift->internal_body(
-	 vs_prize_list(ClubPrizeList => [qw(THIS_DETAIL CLUB_PRIZE)]));
+    return shift->internal_put_base_attr(
+	_tools_prizes('CLUB_PRIZE_LIST'),
+	body => vs_prize_list(ClubPrizeList => [qw(THIS_DETAIL CLUB_PRIZE)]),
+    );
 }
 
 sub prize_select {
      return shift->internal_put_base_attr(
-	tools => TaskMenu([
+	_tools('CLUB_FREIKER_PRIZE_SELECT', [
+	    'CLUB_PRIZE_COUPON_LIST',
 	    'CLUB_FREIKER_SELECT',
 	    {
 		task_id => 'CLUB_FREIKER_MANUAL_RIDE_FORM',
@@ -121,6 +114,44 @@ sub register {
 	ClubRegisterForm.Address.zip
 	ClubRegisterForm.Address.country
     }]));
+}
+
+sub ride_fill_form {
+     return shift->internal_put_base_attr(
+	_tools_rides('CLUB_RIDE_FILL_FORM'),
+	body => vs_simple_form(ClubRideFillForm => [
+	    'ClubRideFillForm.Ride.ride_date',
+	]),
+    );
+}
+
+sub _tools {
+    my($curr, $extras) = @_;
+    return (tools => TaskMenu([
+	grep($curr ne $_,
+	    $extras ? @$extras : (),
+	    qw(
+		CLUB_PRIZE_LIST
+		CLUB_PRIZE_COUPON_LIST
+		GREEN_GEAR_LIST
+		CLUB_FREIKER_CODE_IMPORT
+	    ),
+	),
+    ]));
+}
+
+sub _tools_prizes {
+    return _tools(shift, [qw(
+	CLUB_PRIZE_COUPON_LIST
+	CLUB_FREIKER_SELECT
+    )]);
+}
+
+sub _tools_rides {
+    return _tools(shift, [qw(
+	CLUB_RIDE_FILL_FORM
+	CLUB_RIDE_DATE_LIST
+    )]);
 }
 
 1;
