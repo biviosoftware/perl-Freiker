dc2edfa7490c35320f0e6dafa0de28190a483c15
Rob Nagler <github@q33.us>
2009-05-18 23:16:39 +0000

    LOAD_ALL_SIZE

--- Model/ClubFreikerList.pm
+++ Model/ClubFreikerList.pm
@@ -1,10 +1,14 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Freiker::Model::ClubFreikerList;
 use strict;
 use Bivio::Base 'Model.FreikerList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub LOAD_ALL_SIZE {
+    return 10_000;
+}
 
 sub NOT_FOUND_IF_EMPTY {
     return 0;
