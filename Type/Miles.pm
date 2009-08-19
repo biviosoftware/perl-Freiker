1f6e6a3812be67ec72b3bdfc237db3a0a532c9d9
Rob Nagler <github@q33.us>
2009-08-19 02:24:01 +0000

    v

---
+++ Type/Miles.pm
@@ -0,0 +1,27 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Type::Miles;
+use strict;
+use Bivio::Base 'Type.Kilometers';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub can_be_negative {
+    return 0;
+}
+
+sub can_be_zero {
+    return 0;
+}
+
+sub get_max {
+    my($proto) = @_;
+    return $proto->to_miles($proto->SUPER::get_max);
+}
+
+sub get_min {
+    my($proto) = @_;
+    return $proto->to_miles($proto->SUPER::get_min);
+}
+
+1;
