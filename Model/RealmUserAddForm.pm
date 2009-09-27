3f69a26d26518c748c56f8bb2badeefe32eb5380
Rob Nagler <github@q33.us>
2009-09-27 00:52:34 +0000

    *** empty log message ***

---
+++ Model/RealmUserAddForm.pm
@@ -0,0 +1,26 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::RealmUserAddForm;
+use strict;
+use Bivio::Base 'Model';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+        visible => [
+	    {
+		name => 'Address.country',
+		constraint => 'NONE',
+	    },
+	    {
+		name => 'Address.zip',
+		constraint => 'NONE',
+	    },
+	],
+    });
+}
+
+1;
