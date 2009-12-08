5cb97c95eb183e10be3eef8c32ff06929feae45f
Rob Nagler <github@q33.us>
2009-12-08 21:44:24 +0000

    create_zap

--- Util/Freikometer.pm
+++ Util/Freikometer.pm
@@ -1,20 +1,21 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Freiker::Util::Freikometer;
 use strict;
-use base 'Bivio::ShellUtil';
+use Bivio::Base 'Bivio.ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_FP) = __PACKAGE__->use('Type.FilePath');
-my($_DT) = __PACKAGE__->use('Type.DateTime');
-my($_F) = __PACKAGE__->use('IO.File');
-my($_R) = __PACKAGE__->use('IO.Ref');
+my($_FP) = b_use('Type.FilePath');
+my($_DT) = b_use('Type.DateTime');
+my($_F) = b_use('IO.File');
+my($_R) = b_use('IO.Ref');
 
 sub USAGE {
     return <<'EOF';
 usage: fr-freikometer [options] command [args..]
 commands
   create name -- creates freikometer in realm
+  create_zap name ethernet -- creates dero zap in realm
   do_all method args -- runs method with args on all fms
   do_command cmd ... -- runs cmd(s) on freikometer in shell
   download file ... -- downloads files to freikometer realm
@@ -28,35 +29,27 @@ EOF
 sub create {
     my($self, $name) = @_;
     my($req) = $self->initialize_fully;
-    $self->usage_error('name must begin with fm_')
-	unless $name =~ /^fm_\w+$/;
-    $self->usage_error('you must set a club with -realm')
-	unless $req->get_nested(qw(auth_realm type))->eq_club;
-    my($p) = $self->use('Bivio::Biz::Random')->hex_digits(8);
-    my($ra) = $self->new_other('RealmAdmin');
-    $req->with_realm(undef, sub {
-	$ra->create_user($req->format_email($name), $name, $p, $name);
-    });
-    $ra->join_user('FREIKOMETER');
-    $req->set_realm($req->get('auth_user'));
-    $self->model('Email')->load_for_auth_user->update({want_bulletin => 0});
-    $self->model('RealmFile')->init_realm->map_invoke(
-	create_folder => [
-	    map([{path => $self->model($_)->FOLDER}],
-		qw(FreikometerDownloadList FreikometerUploadList)),
-	],
-    );
+    my($password) = _create($self, 'fm', $name, $name);
     return [
 	$req->is_production ? 'https://fm.freikometer.net'
 	    . $req->format_uri('BOT_FREIKOMETER_UPLOAD')
 	    : $req->format_http('BOT_FREIKOMETER_UPLOAD'),
 	$name,
-	$p,
+	$password
     ];
 }
 
+sub create_zap {
+    sub CREATE_ZAP {[[qw(name RealmName)], [qw(ethernet MACAddress)]]}
+    my($self, $bp) = shift->parameters(\@_);
+    $self->usage_error($bp->{ethernet}, ': ethernet already registered')
+	if $self->model('RealmOwner')
+        ->unauth_load({display_name => $bp->{ethernet}});
+    return _create($self, 'dz', $bp->{name}, $bp->{ethernet});
+}
+
 sub do_all {
-    my($self, $method, @args) = shift->name_args(['Text'], \@_);
+    my($self, $method_or_op, @args) = shift->name_args(['Text'], \@_);
     return join(
 	"\n",
 	@{$self->model(
@@ -64,9 +57,17 @@ sub do_all {
 		: 'FreikometerList',
 	)->map_iterate(sub {
             my($n) = shift->get('RealmOwner.name');
-	    my($res) = $self->req->with_realm($n, sub {$self->$method(@args)});
+	    my($res) = $self->req->with_realm(
+		$n,
+		sub {
+		    return ref($method_or_op) ? $method_or_op->(@args)
+			: $self->$method_or_op(@args);
+		},
+	    );
 	    return "$n:\n"
-		. (ref($res) ? ${Bivio::IO::Ref->to_string($res)} : $res)
+		. (ref($res) ? ${Bivio::IO::Ref->to_string($res)}
+		       : defined($res) ? $res
+		       : '')
 		. "\n";
 	})},
     );
@@ -143,6 +144,10 @@ sub download_playlist {
     );
 }
 
+sub join_user_as_member {
+    return shift->new_other('RealmAdmin')->join_user('MEMBER');
+}
+
 sub list_download {
     return _list('Download', @_);
 }
@@ -157,6 +162,36 @@ sub reboot {
 	content => \('reboot'),
 	content_type => 'text/plain',
     });
+}
+
+sub _create {
+    my($self, $prefix, $name, $display_name) = @_;
+    my($req) = $self->req;
+    $self->usage_error('name must begin with ${prefix}_')
+	unless $name =~ /^${prefix}_\w+$/;
+    $self->usage_error('you must set a club with -realm')
+	unless $req->get_nested(qw(auth_realm type))->eq_club;
+    my($p) = b_use('Bivio::Biz::Random')->hex_digits(8);
+    my($ra) = $self->new_other('RealmAdmin');
+    $req->with_realm(undef, sub {
+        return $ra->create_user(
+	    $req->format_email($name), $display_name, $p, $name);
+    });
+    $ra->join_user('FREIKOMETER');
+    $req->set_realm($req->get('auth_user'));
+    $self->new_other('RealmRole')->edit_categories('+feature_file');
+    $self->new_other('RealmRole')->edit(qw(MEMBER -DATA_WRITE));
+    $self->new_other('RealmRole')->do_super_users(
+	sub {$self->join_user_as_member},
+    );
+    $self->model('Email')->load_for_auth_user->update({want_bulletin => 0});
+    $self->model('RealmFile')->init_realm->map_invoke(
+	create_folder => [
+	    map([{path => $self->model($_)->FOLDER}],
+		qw(FreikometerDownloadList FreikometerUploadList)),
+	],
+    );
+    return $p;
 }
 
 sub _list {
