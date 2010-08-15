857b279740f86e109056f532f532ad155bf238c7
Rob Nagler <github@q33.us>
2010-08-15 18:01:14 +0000

    *** empty log message ***

---
+++ Action/ClientRedirect.pm
@@ -0,0 +1,21 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Action::ClientRedirect;
+use strict;
+use Bivio::Base 'Action';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub uri_parameters {
+    my($proto, $uri) = @_;
+    return {
+	task_id => 'CLIENT_REDIRECT',
+	carry_query => 0,
+	carry_path_info => 0,
+	query => {
+	    $proto->QUERY_TAG => $uri,
+	},
+    };
+}
+
+1;
