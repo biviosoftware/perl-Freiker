bd36a16d543a33a29fb059df5777cf8b6e3b34ea
Rob Nagler <github@q33.us>
2010-08-15 18:00:51 +0000

    don't show FREIKOMETER unless super user

---
+++ Model/GroupUserList.pm
@@ -0,0 +1,19 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::GroupUserList;
+use strict;
+use Bivio::Base 'Model';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    return 0
+	unless shift->SUPER::internal_post_load_row(@_);
+    return $self->req->is_super_user
+	|| !grep($_->eq_freikometer, @{$row->{roles}})
+	? 1 : 0;
+	
+}
+
+1;
