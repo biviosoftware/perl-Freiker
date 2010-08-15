09cf60583454f92cf58aa47cac4bbd15bac47ecb
Rob Nagler <github@q33.us>
2010-08-15 18:01:02 +0000

    limit roles

---
+++ Model/GroupUserQueryForm.pm
@@ -0,0 +1,22 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::GroupUserQueryForm;
+use strict;
+use Bivio::Base 'Model';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_roles {
+    return [grep(
+	$_->equals_by_name(qw(
+	    USER
+	    MEMBER
+	    GUEST
+	    ADMINISTRATOR
+	    UNAPPROVED_APPLICANT
+        )),
+	@{shift->SUPER::internal_roles(@_)},
+    )];
+}
+
+1;
