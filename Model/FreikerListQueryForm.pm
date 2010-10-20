de16918b20256a8af05bd43cc289fca50e66cba5
Rob Nagler <github@q33.us>
2010-10-20 20:49:59 +0000

    factored out login from FreikerList->_get_from_query to FreikerListQueryForm->get_value_fr

--- Model/FreikerListQueryForm.pm
+++ Model/FreikerListQueryForm.pm
@@ -5,14 +5,28 @@ use strict;
 use Bivio::Base 'Model.ListQueryForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_YQ) = b_use('Type.YearQuery');
+my($_B) = b_use('Type.Boolean');
+my($_D) = b_use('Type.Date');
 
 sub filter_keys {
     return [qr{^fr_\w+$}, @{shift->SUPER::filter_keys(@_)}];
 }
 
+sub get_value_fr {
+    my($proto, $list, $which) = @_;
+    return undef
+	unless my $v = $list->ureq('Model.FreikerListQueryForm', $which)
+	|| $list->get_query->unsafe_get($which);
+    return $which eq 'fr_year' ? $_YQ->unsafe_from_any($v)
+	: $which =~ /^fr_(?:begin|end)/ ? ($_D->from_literal($v))[0]
+        : ($_B->from_literal($v))[0];
+}
+
 sub internal_query_fields {
     my($self) = @_;
     return [
+	[qw(fr_all TripsQuery)],
 	[qw(fr_registered Boolean)],
 	[qw(fr_trips TripsQuery)],
 	[qw(fr_year YearQuery)],
