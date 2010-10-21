53c2b18b2bbb536fd2b14936b385f62d5f484d1c
Rob Nagler <github@q33.us>
2010-10-21 14:00:47 +0000

    modified_date_time is now max of all uploaded files

--- Model/AdmFreikometerList.pm
+++ Model/AdmFreikometerList.pm
@@ -5,6 +5,7 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_FOLDER_RE) = b_use('Model.FreikometerUploadList')->FOLDER_LC . '%';
 
 sub internal_initialize {
     my($self) = @_;
@@ -15,15 +16,43 @@ sub internal_initialize {
 	order_by => [
 	    'RealmOwner.name',
 	    'RealmOwner.display_name',
-	    'RealmFile.modified_date_time',
+	    {
+		name => 'modified_date_time',
+		type => 'DateTime',
+		in_select => 1,
+		select_value =>
+		    b_use('Type.DateTime')
+		        ->from_sql_value('MAX(realm_file_t.modified_date_time)')
+		        . ' AS modified_date_time',
+	    },
+	],
+	group_by => [
+	    'RealmOwner.name',
+	    'RealmOwner.display_name',
 	],
 	other => [
 	    ['RealmUser.role', ['FREIKOMETER']],
-#TODO: need a max time on upload recursively
-	    ['RealmFile.path_lc', [
-		$self->get_instance('FreikometerUploadList')->FOLDER_LC]],
+	    ['RealmFile.is_folder', [0]],
+	    $self->field_decl(
+		[qw(
+		    RealmFile.realm_file_id
+		    RealmUser.role
+		    RealmUser.realm_id
+		    RealmFile.is_folder
+		    RealmOwner.realm_id
+		)],
+		{in_select => 0},
+	    ),
 	],
     });
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt) = @_;
+    $stmt->where(
+	$stmt->LIKE('RealmFile.path_lc', $_FOLDER_RE),
+    );
+    return shift->SUPER::internal_prepare_statement(@_);
 }
 
 1;
