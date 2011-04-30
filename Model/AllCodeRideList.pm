6a82f9e98d171601159057d857130c6e862c41a9
Rob Nagler <github@q33.us>
2011-04-30 17:34:51 +0000

    *** empty log message ***

---
+++ Model/AllCodeRideList.pm
@@ -0,0 +1,40 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::AllCodeRideList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	order_by => [qw(
+	    FreikerCode.freiker_code
+	    Ride.ride_date
+	)],
+	primary_key => [qw(
+	    FreikerCode.freiker_code
+	    Ride.ride_date
+	)],
+	other => [
+	    [
+		{
+		    name => 'Ride.user_id',
+		    in_select => 0,
+		},
+		'FreikerCode.user_id',
+	    ],
+	],
+        auth_id => [
+	    {
+		name => 'Ride.club_id',
+		in_select => 0,
+	    },
+	    'FreikerCode.club_id',
+	]
+    });
+}
+
+1;
