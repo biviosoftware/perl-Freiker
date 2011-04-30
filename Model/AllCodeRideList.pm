1e7f413eb47bd9e3e70bea99bfdd5a4565c89773
Rob Nagler <github@q33.us>
2011-04-30 18:09:00 +0000

    can_iterate

--- Model/AllCodeRideList.pm
+++ Model/AllCodeRideList.pm
@@ -10,6 +10,7 @@ sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
+	can_iterate => 1,
 	order_by => [qw(
 	    FreikerCode.freiker_code
 	    Ride.ride_date
