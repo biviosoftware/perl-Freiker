5bb8921d184f94177f4041ce1922dbbcd053375c
Rob Nagler <github@q33.us>
2011-09-18 03:10:51 +0000

    *** empty log message ***

---
+++ Model/GroupUserBulletinList.pm
@@ -0,0 +1,31 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::GroupUserBulletinList;
+use strict;
+use Bivio::Base 'Model.GroupUserList';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_E) = b_use('Type.Email');
+my($_F) = b_use('ShellUtil.Freikometer');
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+        other => [
+	    ['Email.want_bulletin', [1]],
+	],
+    });
+}
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    return 0
+	unless shift->SUPER::internal_post_load_row(@_);
+    return 0
+	if $_E->is_ignore($row->{'Email.email'})
+	|| $_F->is_freikometer_name($row->{'RealmOwner.name'});
+    return 1;
+}
+
+1;
---
+++ View/GroupAdmin.pm
@@ -0,0 +1,27 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::View::GroupAdmin;
+use strict;
+use Bivio::Base 'View';
+use Bivio::UI::ViewLanguageAUTOLOAD;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub user_bulletin_list_csv {
+    return shift->internal_body(
+	CSV(
+	    'GroupUserBulletinList',
+	    [qw(
+	        Email.email
+		User.first_name
+		User.middle_name
+		User.last_name
+	    )],
+            {
+	        want_iterate_start => 1,
+	    },
+	),
+    );
+}
+
+1;
