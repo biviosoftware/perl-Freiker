e4715210f08fe739cfc0725cca6c30f413736586
Justin Schell <schellj@bivio.biz>
2011-09-23 22:38:49 +0000

    rework to be extendable to clubs

--- Model/AdmRideList.pm
+++ Model/AdmRideList.pm
@@ -5,47 +5,37 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_K) = b_use('Type.Kilometers');
 my($_FLQF) = b_use('Model.FreikerListQueryForm');
-my($_D) = b_use('Type.Date');
-my($_FL) = b_use('Model.FreikerList');
+
+sub LOAD_ALL_SIZE {
+    return 10_000_000;
+}
+
+sub AUTH_ID {
+    return undef;
+}
+
+sub OTHER_COLUMNS {
+    return (
+	[qw(Ride.club_id RealmOwner.realm_id)],
+	'RealmOwner.realm_id',
+	'RealmOwner.display_name',
+    );
+}
 
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
-	primary_key => [[qw(Ride.user_id FreikerInfo.user_id)]],
+	primary_key => [qw(Ride.user_id Ride.ride_date)],
 	other => [
-	    qw(
-		Ride.ride_date
-		FreikerInfo.distance_kilometers
-	    ),
-	    $self->field_decl([
-		[qw(ride_count Integer)],
-		[qw(current_miles Miles)],
-		[qw(current_kilometers Kilometers)],
-		[qw(calories Integer)],
-		[qw(co2_saved Integer)],
-	    ]),
+	    $self->OTHER_COLUMNS,
+	    [qw(Ride.user_id FreikerInfo.user_id)],
+	    'FreikerInfo.distance_kilometers',
 	],
 	other_query_keys => [qw(fr_begin fr_end)],
+	auth_id => $self->AUTH_ID,
     });
-}
-
-sub internal_post_load_row {
-    my($self, $row) = @_;
-    return 0
-	unless shift->SUPER::internal_post_load_row(@_);
-    $row->{ride_count} = 1;
-    $row->{current_kilometers} = $row->{'FreikerInfo.distance_kilometers'}
-	&& 2 * $row->{'FreikerInfo.distance_kilometers'};
-    $row->{current_miles} = $row->{'FreikerInfo.distance_kilometers'}
-	&& $_K->to_miles($row->{current_kilometers});
-    $row->{calories} = $row->{current_miles}
-	&& $row->{current_miles} * $_FL->CALORIES_PER_MILE;
-    $row->{co2_saved} = $row->{current_miles}
-	&& $row->{current_miles} * $_FL->CO2_POUNDS_PER_MILE;
-    return 1;
 }
 
 sub internal_prepare_statement {
