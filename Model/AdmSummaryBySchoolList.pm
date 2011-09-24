13d436801312e004583f1daaf2c5705dbc6c6d51
Justin Schell <schellj@bivio.biz>
2011-09-24 01:04:33 +0000

    reworked to load in a more acceptable time frame

--- Model/AdmSummaryBySchoolList.pm
+++ Model/AdmSummaryBySchoolList.pm
@@ -5,16 +5,16 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_RT) = b_use('Auth.RealmType');
+my($_K) = b_use('Type.Kilometers');
+my($_FL) = b_use('Model.FreikerList');
 
-sub REALM_TYPE {
-    return $_RT->CLUB;
+sub FIELDS {
+    return qw(RealmOwner.realm_id
+	      RealmOwner.display_name);
 }
 
-sub internal_get_summary {
-    my($self, $realm_id) = @_;
-    $self->req->set_realm($realm_id);
-    return $self->new_other('ClubFreikerList')->load_all->get_summary;
+sub RIDE_MODEL {
+    return 'AdmRideList';
 }
 
 sub internal_initialize {
@@ -23,34 +23,43 @@ sub internal_initialize {
         version => 1,
         primary_key => ['RealmOwner.realm_id'],
 	other => [
-	    ['RealmOwner.realm_type', [$self->REALM_TYPE]],
-	    'RealmOwner.display_name',
 	    $self->field_decl([
+		[qw(realm_display_name String)],
 		[qw(ride_count Integer)],
-		[qw(current_miles Miles)],
+		[qw(current_miles Integer)],
 		[qw(current_kilometers Kilometers)],
 		[qw(calories Integer)],
 		[qw(co2_saved Integer)],
 	    ]),
 	],
-	other_query_keys => [qw(fr_begin fr_end)],
     });
 }
 
-sub internal_post_load_row {
-    my($self, $row) = @_;
-    return 0
-	unless shift->SUPER::internal_post_load_row(@_);
-    my($ro) = $self->new_other('RealmOwner');
-    $ro->unauth_load({
-	realm_id => $row->{'RealmOwner.realm_id'},
+sub internal_load_rows {
+    my($self) = @_;
+    my($realms) = {};
+    # ->load_all->do_rows is faster than ->do_iterate for large data sets
+    $self->new_other($self->RIDE_MODEL)->load_all->do_rows(sub {
+	my($rid, $dn, $d) = shift->get($self->FIELDS, 'FreikerInfo.distance_kilometers');
+	$rid ||= 0;
+	$d ||= 0;
+	($realms->{$rid} ||= {})->{'RealmOwner.realm_id'} ||= $rid;
+	$realms->{$rid}->{'realm_display_name'} ||= $dn;
+	($realms->{$rid}->{'ride_count'} ||= 0)++;
+	($realms->{$rid}->{'current_kilometers'} ||= 0) += 2 * $d;
+	return 1;
     });
-    return 0
-	if $ro->is_default;
-    my($s) = $self->internal_get_summary($row->{'RealmOwner.realm_id'});
-    map($row->{$_} = $s->get($_),
-	qw(ride_count current_miles current_kilometers calories co2_saved));
-    return 1;
+    foreach my $r (keys(%$realms)) {
+	my($m) = $_K->to_miles($realms->{$r}->{'current_kilometers'});
+	$realms->{$r}->{'current_miles'} = $m;
+	$realms->{$r}->{'calories'} = $m * $_FL->CALORIES_PER_MILE;
+	$realms->{$r}->{'co2_saved'} = $m * $_FL->CO2_POUNDS_PER_MILE;
+    }
+    my($rows) = [];
+    map(push(@$rows, $realms->{$_}),
+	sort({$realms->{$a}->{'realm_display_name'}
+		  cmp $realms->{$b}->{'realm_display_name'}} keys(%$realms)));
+    return $rows;
 }
 
 1;
