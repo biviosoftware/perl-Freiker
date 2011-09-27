bc18425460fbd651c65c7869caab2a25f1583f3e
Justin Schell <schellj@bivio.biz>
2011-09-27 02:03:03 +0000

    handle numeric prefixes

--- Delegate/RealmName.pm
+++ Delegate/RealmName.pm
@@ -9,9 +9,13 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub from_display_name_and_zip {
     my($proto, $display_name, $zip) = @_;
     my($n) = $display_name;
+    $n =~ s/^((\d|\W)*)//;
+    my($numeric_prefix) = $1;
+    $numeric_prefix =~ s/\D+//g;
     $n =~ s/^(?:the|an|a|saint|santa)\s+//i;
     $n =~ s/\b(co|corp|company|inc|ltd|llc)\.?$//i;
     $n =~ s/\s+.*|\W+//g;
+    $n .= $numeric_prefix;
     $zip =~ s/[^a-z0-9]//ig;
     return lc(substr($n, 0, $proto->get_width - length($zip)) . $zip);
 }
