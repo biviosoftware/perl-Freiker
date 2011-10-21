5cac8a2e3b849a308f9fe098bbc1a3f7f0a9cf8b
Justin Schell <schellj@bivio.biz>
2011-10-21 22:54:07 +0000

    *** empty log message ***

---
+++ Model/AdmFreikometerFileList.pm
@@ -0,0 +1,42 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Model::AdmFreikometerFileList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DT) = b_use('Type.DateTime');
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	can_iterate => 1,
+	primary_key => ['RealmFile.realm_file_id'],
+        order_by => [
+	    'RealmFile.modified_date_time',
+	],
+	other => [
+	    ['RealmUser.role', ['FREIKOMETER']],
+	    [qw(RealmFile.realm_id RealmUser.user_id)],
+	    'RealmFile.path_lc',
+	    'RealmFile.is_folder',
+	],
+	auth_id => 'RealmFile.realm_id',
+    });
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt, $query) = @_;
+    $stmt->where(
+	$stmt->AND(
+	    $stmt->LT('RealmFile.modified_date_time',
+		      [$query->unsafe_get('date') || $_DT->now]),
+	    $stmt->GT('RealmFile.modified_date_time',
+		      [$query->unsafe_get('begin_date') || $_DT->get_min]),
+	),
+    );
+    return;
+}
+
+1;
---
+++ Util/RealmFile.pm
@@ -0,0 +1,52 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Util::RealmFile;
+use strict;
+use Bivio::Base 'ShellUtil';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_LQ) = b_use('SQL.ListQuery');
+my($_DT) = b_use('Type.DateTime');
+
+sub DAYS_TO_KEEP_FREIKOMETER_FILES {
+    return 180;
+}
+
+sub purge_freikometer_files {
+    my($self) = @_;
+    my($count) = 0;
+    my($realms) = {};
+    $self->model('AdmFreikometerFileList')->do_iterate(
+	sub {
+	    my($it) = @_;
+	    $realms->{$it->get('RealmFile.realm_id')} = 1;
+	    my($rf) = $it->new_other('RealmFile');
+	    $rf->unauth_load({
+		realm_file_id => $it->get('RealmFile.realm_file_id'),
+	    });
+	    return 1
+		if $it->get('RealmFile.is_folder');
+	    $rf->delete({
+		override_is_read_only => 1,
+		override_versioning => 1,
+	    });
+	    $count++;
+	    $self->commit_or_rollback
+		if $count % 100 == 0;
+	    return 1;
+	},
+	'unauth_iterate_start',
+	{
+	    $_LQ->to_char('date') => $_DT->add_days(
+		$_DT->now, -$self->DAYS_TO_KEEP_FREIKOMETER_FILES),
+	},
+    );
+    for my $r (keys(%$realms)) {
+	$self->req->with_realm($r, sub {
+	    $self->model('RealmFile')->delete_empty_folders;
+	});
+    }
+    return $count;
+}
+
+1;
