0a0a55fc0fac28d730dd8ff1eae88a5a190b4ddc
Justin Schell <schellj@bivio.biz>
2012-04-19 01:34:09 +0000

    assert_access here instead of in ManualRideListForm

--- Model/ManualRideList.pm
+++ Model/ManualRideList.pm
@@ -5,9 +5,16 @@ use strict;
 use Bivio::Base 'Model.NumberedList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_FRL) = b_use('Model.FreikerRideList');
 
 sub LOAD_ALL_SIZE {
     return 0;
+}
+
+sub internal_prepare_statement {
+    my($self) = @_;
+    $_FRL->assert_access($self, $self->get_query->get('parent_id'));
+    return shift->SUPER::internal_prepare_statement(@_);
 }
 
 1;
