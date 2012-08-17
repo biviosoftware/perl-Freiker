52d6b691388fc1ea853d29ed88ae2ae7b9b5a860
Justin Schell <schellj@bivio.biz>
2012-08-17 16:43:43 +0000

    *** empty log message ***

---
+++ Action/Auth.pm
@@ -0,0 +1,17 @@
+# Copyright (c) 2012 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Freiker::Action::Auth;
+use strict;
+use Bivio::Base 'Biz.Action';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DC) = b_use('Bivio.DieCode');
+
+sub execute_is_sub_or_super_user {
+    my($proto, $req) = @_;
+    $_DC->FORBIDDEN->throw_die('must be substitute or super user')
+	unless $req->is_super_user || $req->is_substitute_user;
+    return;
+}
+
+1;
