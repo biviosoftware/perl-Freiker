e988d280d28be7ccdff661bef47634c8a37a005f
Justin Schell <schellj@bivio.biz>
2012-08-24 16:19:32 +0000

    load models from pm.  some schools on production don't have them in the database.

--- Delegate/TaskId.pm
+++ Delegate/TaskId.pm
@@ -623,9 +623,6 @@ sub get_delegate_info {
 	    ADMIN_READ&ADMIN_WRITE
 	    Action.Auth->execute_is_sub_or_super_user
 	    Type.FormMode->execute_edit
-	    Model.Address->execute_load
-	    Model.SchoolContact->execute_load
-	    Model.Website->execute_load
 	    Model.ClubRegisterForm
 	    View.Club->register
 	    require_context=1
