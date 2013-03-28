ce020dfa6fc414e1e2df053d93dbd3c838c44e02
Paul Moeller <moeller@bivio.biz>
2013-03-28 16:42:47 +0000

    added school_year() export to create zip for all schools in school year

--- Util/Export.pm
+++ Util/Export.pm
@@ -14,15 +14,61 @@ sub USAGE {
     return <<'EOF';
 usage: bivio Export [options] command [args..]
 commands
+  school_year year -- creates a zip file with all schools for the school year
   school_zip year -- creates zip file for school year data
 EOF
 }
 
+sub school_year {
+    my($self, $year) = @_;
+    $year = _parse_year($self, $year);
+    my($school_year) = $self->model('SchoolYear');
+    my($file_names) = [];
+    $self->model('Club')->do_iterate(
+	sub {
+	    my($school) = @_;
+	    return 1
+		unless $school_year->unauth_load({
+		    club_id => $school->get('club_id'),
+		    start_date => $_D->date_from_parts(
+			1, $_YQ->START_MONTH, $year),
+		});
+	    my($file_name) = $self->req->with_realm(
+		$school->get('club_id'),
+		sub {
+		    return $self->school_zip($year);
+		});
+
+	    if ($self->get('ride_count') && $self->get('user_count')) {
+		push(@$file_names, $file_name);
+	    }
+	    else {
+		$self->print(
+		    'no rides or users, removing: ', $file_name, "\n");
+		unlink($file_name);
+	    }
+	    return 1;
+	},
+	'unauth_iterate_start',
+    );
+    my($zip) = b_use('IO.Zip')->new;
+
+    foreach my $file (@$file_names) {
+	$zip->add_file($file, $file);
+    }
+    my($outfile) = _outfile($self, 'school_year', $year);
+    $zip->write_to_file($outfile);
+
+    foreach my $file (@$file_names) {
+	unlink($file);
+    }
+    $self->print('created export file: ', $outfile, "\n");
+    return $outfile;
+}
+
 sub school_zip {
     my($self, $year) = @_;
-    $self->usage_error('missing year')
-	unless $year;
-    $year = b_use('Type.Year')->from_literal_or_die($year);
+    $year = _parse_year($self, $year);
     my($school_year) = $self->model(SchoolYear => {
 	start_date => $_D->date_from_parts(
 	    1, $_YQ->START_MONTH, $year),
@@ -32,12 +78,11 @@ sub school_zip {
     _add_to_zip($zip, _rides($self, $school_year), 'rides.csv');
     _add_to_zip($zip, _classes($self, $school_year), 'classes.csv');
     _add_to_zip($zip, _users($self, $school_year), 'users.csv');
-    my($outfile) = $self->req(qw(auth_realm owner name))
-	. '-' . $_YQ->compile_short_desc($year) . '.zip';
-    $outfile =~ s/\s+//g;
+    my($outfile) = _outfile(
+	$self, $self->req(qw(auth_realm owner name)), $year);
     $zip->write_to_file($outfile);
     $self->print('created export file: ', $outfile, "\n");
-    return;
+    return $outfile;
 }
 
 sub _add_row {
@@ -82,10 +127,25 @@ sub _classes {
     return $rows;
 }
 
+sub _outfile {
+    my($self, $name, $year) = @_;
+    my($res) = $name . '-' . $_YQ->compile_short_desc($year) . '.zip';
+    $res =~ s/\s+//g;
+    return $res;
+}
+
+sub _parse_year {
+    my($self, $year) = @_;
+    $self->usage_error('missing year')
+	unless $year;
+    return b_use('Type.Year')->from_literal_or_die($year);
+}
+
 sub _rides {
     my($self, $school_year) = @_;
     my($user_ids) = {};
     $self->put(user_ids => $user_ids);
+    my($ride_count) = 0;
     my($rows) = [
 	[qw(user_id ride_date ride_time ride_type)],
     ];
@@ -101,6 +161,7 @@ sub _rides {
 			$ride->get('ride_date'), $end);
 	    $user_ids->{$ride->get('user_id')} = 1;
 	    _add_row($self, $rows, $ride);
+	    $ride_count++;
 	    return 1;
 	},
 	'unauth_iterate_start',
@@ -109,6 +170,7 @@ sub _rides {
 	    club_id => $self->req('auth_id'),
 	},
     );
+    $self->put(ride_count => $ride_count);
     foreach my $row (@$rows) {
 	next if $row->[1] eq 'ride_date';
 	$row->[1] = $_D->to_string($row->[1]);
@@ -137,20 +199,23 @@ sub _users {
 	[qw(user_id realm_id distance_kilometers)],
     ];
     my($info) = $self->model('FreikerInfo');
+    my($user_count) = 0;
 
     foreach my $class_id (@$class_ids) {
 	$self->model('RealmUser')->do_iterate(
 	    sub {
 		my($realm_user) = @_;
-		_add_row(
-		    $self, $rows, $realm_user,
-		    $info->unauth_load({
-			user_id => $realm_user->get('user_id'),
-		    })
-			? $info
-		        : (),
-		    )
-		    if $user_ids->{$realm_user->get('user_id')};
+		if ($user_ids->{$realm_user->get('user_id')}) {
+		    _add_row(
+			$self, $rows, $realm_user,
+			$info->unauth_load({
+			    user_id => $realm_user->get('user_id'),
+			})
+			    ? $info
+			    : (),
+		    );
+		    $user_count++;
+		}
 		return 1;
 	    },
 	    'unauth_iterate_start',
@@ -159,6 +224,7 @@ sub _users {
 	    },
 	);
     }
+    $self->put(user_count => $user_count);
     $rows->[0]->[1] = 'school_class_id';
     return $rows;
 }
