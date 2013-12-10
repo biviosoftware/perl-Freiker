02b8796c2d7803870fb59e0834c552cd7beabf0d
Justin Schell <schellj@bivio.biz>
2013-12-10 00:58:58 +0000

    move create_mail, unknown school contact label into facade

--- View/UserAuth.pm
+++ View/UserAuth.pm
@@ -23,38 +23,6 @@ sub create {
     ]));
 }
 
-sub create_mail {
-    view_put(
-	mail_to => Mailbox([['->get_by_regexp', 'Model.\w+RegisterForm'], 'Email.email']),
-	mail_recipients => Mailbox([['->get_by_regexp', 'Model.\w+RegisterForm'], 'Email.email']),
-	mail_subject => Join([vs_site_name(), ' Registration Verification']),
-	mail_body => Prose(<<'EOF'),
-Thank you for registering with vs_site_name();.  In order to
-complete your registration, please click on the following link:
-
-String([['->get_by_regexp', 'Model.\w+RegisterForm'], 'uri']);
-
-String([['->get_by_regexp', 'Model.\w+RegisterForm'], 'Email.email']); is the email address we have for you in our database.
-You will use this address to login along with the password you
-will set when you click on the above link.
-
-Thanks for registering.  Every Trip Counts!TM
-
-KidCommute, Inc.
-Link('SITE_ROOT');
-
-KidCommute, Inc. (EIN 56-2539016) is a tax-exempt organization under Section
-501(c)3 of the Internal Revenue Code. Your donation is tax deductible to the
-full extent of the law.  Just $10 will help:
-KidCommute, Inc. (EIN 56-2539016) is a tax-exempt organization under
-Section 501(c)(3) of the Internal Revenue Code. Your donation is tax
-deductible to the full extent of the law - Link('/site/donate');
-
-EOF
-    );
-    return;
-}
-
 sub general_contact {
     return shift->internal_body(vs_simple_form(ContactForm => [
         'ContactForm.from',
@@ -63,7 +31,7 @@ sub general_contact {
 	    choices => ['Model.SchoolContactList'],
 	    list_id_field => 'SchoolContact.club_id',
 	    list_display_field => 'email_display_name',
-	    unknown_label => 'Select School Contact',
+	    unknown_label => vs_text('SchoolContactList.unknown_label'),
 	}],
 	'ContactForm.subject',
 	'ContactForm.text',
